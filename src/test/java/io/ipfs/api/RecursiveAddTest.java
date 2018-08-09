0a6b7114fe952a8e388fcfe0c44fcfbf05c509a7
Ian Preston <ianopolous@gmail.com>
2018-08-09 20:12:12 +0100

    add test for larger binary data add

--- src/test/java/io/ipfs/api/RecursiveAddTest.java
+++ src/test/java/io/ipfs/api/RecursiveAddTest.java
@@ -48,4 +48,24 @@ public class RecursiveAddTest {
         MerkleNode node = add.get(add.size() - 1);
         Assert.assertEquals(EXPECTED, node.hash.toBase58());
     }
+
+    @Test
+    public void largeBinaryRecursiveAdd() throws Exception {
+        String EXPECTED = "QmZdfdj7nfxE68fBPUWAGrffGL3sYGx1MDEozMg73uD2wj";
+
+        Path base = Paths.get("tmplargebindata");
+        base.toFile().mkdirs();
+        byte[] bindata = new byte[100 * 1024*1024];
+        new Random(28).nextBytes(bindata);
+        Files.write(base.resolve("data.bin"), bindata);
+        new Random(496).nextBytes(bindata);
+        Files.write(base.resolve("data2.bin"), bindata);
+        Path js = base.resolve("js");
+        js.toFile().mkdirs();
+        Files.write(js.resolve("func.js"), "function() {console.log('Hey');}".getBytes());
+
+        List<MerkleNode> add = ipfs.add(new NamedStreamable.FileWrapper(base.toFile()));
+        MerkleNode node = add.get(add.size() - 1);
+        Assert.assertEquals(EXPECTED, node.hash.toBase58());
+    }
 }
