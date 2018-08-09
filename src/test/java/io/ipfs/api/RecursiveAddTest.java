85d643bd0e585ce23d603988d3079f7595c8ec93
Ian Preston <ianopolous@gmail.com>
2018-08-09 19:39:02 +0100

    fix test

--- src/test/java/io/ipfs/api/RecursiveAddTest.java
+++ src/test/java/io/ipfs/api/RecursiveAddTest.java
@@ -2,6 +2,7 @@ package io.ipfs.api;
 
 import java.io.*;
 import java.nio.file.*;
+import java.util.*;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,14 +15,19 @@ public class RecursiveAddTest {
     
     @Test
     public void testAdd() throws Exception {
-        
-        String TEST_HASH = "QmWVMRbFTrA6pMhsfXzsyoWqmrhy5FPd4VitfX79R5bguw";
-        
         System.out.println("ipfs version: " + ipfs.version());
-        System.out.println("pwd: " + new File("").getAbsolutePath());
-        
-        Path path = Paths.get("src/test/resources/html");
-        MerkleNode node = ipfs.add(new NamedStreamable.FileWrapper(path.toFile())).get(0);
-        Assert.assertEquals(TEST_HASH, node.hash.toBase58());
+
+        String EXPECTED = "QmX5fZ6aUxNTAS7ZfYc8f4wPoMx6LctuNbMjuJZ9EmUSr6";
+
+        Path base = Paths.get("tmpdata");
+        base.toFile().mkdirs();
+        Files.write(base.resolve("index.html"), "<html></html>".getBytes());
+        Path js = base.resolve("js");
+        js.toFile().mkdirs();
+        Files.write(js.resolve("func.js"), "function() {console.log('Hey');}".getBytes());
+
+        List<MerkleNode> add = ipfs.add(new NamedStreamable.FileWrapper(base.toFile()));
+        MerkleNode node = add.get(add.size() - 1);
+        Assert.assertEquals(EXPECTED, node.hash.toBase58());
     }
 }
