fe6db8c38c2ed4fb7a6490db89b4a9046a9faa29
ian <ianopolous@protonmail.com>
2020-07-29 08:15:54 +0100

    Upgrade to ipfs v0.6.0 in CI

--- install-run-ipfs.sh
+++ install-run-ipfs.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-wget https://dist.ipfs.io/go-ipfs/v0.5.0/go-ipfs_v0.5.0_linux-amd64.tar.gz -O /tmp/go-ipfs_linux-amd64.tar.gz
+wget https://dist.ipfs.io/go-ipfs/v0.6.0/go-ipfs_v0.6.0_linux-amd64.tar.gz -O /tmp/go-ipfs_linux-amd64.tar.gz
 tar -xvf /tmp/go-ipfs_linux-amd64.tar.gz
 export PATH=$PATH:$PWD/go-ipfs/
 ipfs init
