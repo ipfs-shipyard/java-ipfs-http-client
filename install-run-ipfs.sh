be68ce72376d59ad8753bb267f7d8e26d3470f27
ian <ianopolous@protonmail.com>
2022-10-28 12:27:48 +0100

    test against kubo v0.16.0

--- install-run-ipfs.sh
+++ install-run-ipfs.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-wget https://dist.ipfs.io/kubo/v0.15.0/kubo_v0.15.0_linux-amd64.tar.gz -O /tmp/kubo_linux-amd64.tar.gz
+wget https://dist.ipfs.io/kubo/v0.16.0/kubo_v0.16.0_linux-amd64.tar.gz -O /tmp/kubo_linux-amd64.tar.gz
 tar -xvf /tmp/kubo_linux-amd64.tar.gz
 export PATH=$PATH:$PWD/kubo/
 ipfs init
