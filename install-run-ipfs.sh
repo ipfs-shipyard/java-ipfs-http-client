df73c85926ef27e6930993a70b389eec3b4657ba
Tamas Cservenak <tamas@cservenak.net>
2026-01-19 15:55:13 +0100

    Update Kubo version
    From ancient 0.18.1 to latest 0.39.0. This causes test
    breakage as some deprecated endpoints like `object` are
    dropped.

--- install-run-ipfs.sh
+++ install-run-ipfs.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-wget https://dist.ipfs.io/kubo/v0.18.1/kubo_v0.18.1_linux-amd64.tar.gz -O /tmp/kubo_linux-amd64.tar.gz
+wget https://dist.ipfs.io/kubo/v0.39.0/kubo_v0.39.0_linux-amd64.tar.gz -O /tmp/kubo_linux-amd64.tar.gz
 tar -xvf /tmp/kubo_linux-amd64.tar.gz
 export PATH=$PATH:$PWD/kubo/
-ipfs init
-ipfs daemon --enable-pubsub-experiment --routing=dhtclient &
+ipfs init --profile server
+ipfs daemon --enable-pubsub-experiment --enable-namesys-pubsub --routing=dhtclient &
