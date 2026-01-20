5e0f01f4023cd1000507d9aecd5323c568b186d1
Tamas Cservenak <tamas@cservenak.net>
2026-01-20 23:08:36 +0100

    Auto format source and POM
    Code change in pom.xml:
    * added spotless plugin w/: googleJavaFormat, removeUnusedImports and sortPom
    * added profiles (dev reformat, CI enforce formatting)
    
    Rest of codebase is _reformatted only_ (more precisely, no semantic changes, imports are optimized to remove wildcard imports).

--- src/main/java/io/ipfs/api/demo/UsageRemotePinningAPI.java
+++ src/main/java/io/ipfs/api/demo/UsageRemotePinningAPI.java
@@ -5,7 +5,6 @@ import io.ipfs.api.MerkleNode;
 import io.ipfs.api.NamedStreamable;
 import io.ipfs.multiaddr.MultiAddress;
 import io.ipfs.multihash.Multihash;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -28,48 +27,50 @@ Note: The above parameters are referenced in the program below
  */
 public class UsageRemotePinningAPI {
 
-    public UsageRemotePinningAPI(IPFS ipfsClient) {
-        try {
-            run(ipfsClient);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
+  public UsageRemotePinningAPI(IPFS ipfsClient) {
+    try {
+      run(ipfsClient);
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
     }
-    private void run(IPFS ipfs) throws IOException {
+  }
 
-        // Add  file to the local node
-        MerkleNode file = ipfs.add(new NamedStreamable.ByteArrayWrapper("file.txt", "test data".getBytes())).get(0);
-        // Retrieve CID
-        Multihash hash = file.hash;
+  private void run(IPFS ipfs) throws IOException {
 
-        //Add the service
-        String serviceName = "mock";
-        ipfs.pin.remote.rmService(serviceName); //clean up if necessary
-        ipfs.pin.remote.addService(serviceName, "http://127.0.0.1:3000", "secret");
+    // Add  file to the local node
+    MerkleNode file =
+        ipfs.add(new NamedStreamable.ByteArrayWrapper("file.txt", "test data".getBytes())).get(0);
+    // Retrieve CID
+    Multihash hash = file.hash;
 
-        //List services
-        List<Map> services = ipfs.pin.remote.lsService(true);
-        for(Map service : services) {
-            System.out.println(service);
-        }
+    // Add the service
+    String serviceName = "mock";
+    ipfs.pin.remote.rmService(serviceName); // clean up if necessary
+    ipfs.pin.remote.addService(serviceName, "http://127.0.0.1:3000", "secret");
 
-        // Pin
-        Map addHashResult = ipfs.pin.remote.add(serviceName, hash, Optional.empty(), true);
-        System.out.println(addHashResult);
-
-        // List
-        List<IPFS.PinStatus> statusList = List.of(IPFS.PinStatus.values()); // all statuses
-        Map ls = ipfs.pin.remote.ls(serviceName, Optional.empty(), Optional.of(statusList));
-        System.out.println(ls);
-
-        // Remove pin from remote pinning service
-        List<IPFS.PinStatus> queued = List.of(IPFS.PinStatus.queued);
-        ipfs.pin.remote.rm(serviceName, Optional.empty(), Optional.of(queued), Optional.of(List.of(hash)));
-
+    // List services
+    List<Map> services = ipfs.pin.remote.lsService(true);
+    for (Map service : services) {
+      System.out.println(service);
     }
 
-    public static void main(String[] args) {
-        IPFS ipfsClient = new IPFS(new MultiAddress("/ip4/127.0.0.1/tcp/5001"));
-        new UsageRemotePinningAPI(ipfsClient);
-    }
+    // Pin
+    Map addHashResult = ipfs.pin.remote.add(serviceName, hash, Optional.empty(), true);
+    System.out.println(addHashResult);
+
+    // List
+    List<IPFS.PinStatus> statusList = List.of(IPFS.PinStatus.values()); // all statuses
+    Map ls = ipfs.pin.remote.ls(serviceName, Optional.empty(), Optional.of(statusList));
+    System.out.println(ls);
+
+    // Remove pin from remote pinning service
+    List<IPFS.PinStatus> queued = List.of(IPFS.PinStatus.queued);
+    ipfs.pin.remote.rm(
+        serviceName, Optional.empty(), Optional.of(queued), Optional.of(List.of(hash)));
+  }
+
+  public static void main(String[] args) {
+    IPFS ipfsClient = new IPFS(new MultiAddress("/ip4/127.0.0.1/tcp/5001"));
+    new UsageRemotePinningAPI(ipfsClient);
+  }
 }
--- src/test/java/io/ipfs/api/VersionsTest.java
+++ src/test/java/io/ipfs/api/VersionsTest.java
@@ -1,19 +1,23 @@
 package io.ipfs.api;
 
-import org.junit.*;
-
-import java.util.*;
-import java.util.stream.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class VersionsTest {
 
-    @Test
-    public void sorting(){
-        List<String> original = Arrays.asList("1.0.3", "0.4.9", "0.4.10", "0.5.1-rc1", "0.5.1-rc2", "0.5.1-rc2+meta");
-        List<Version> versions = original.stream().map(Version::parse).collect(Collectors.toList());
-        Collections.sort(versions);
-        List<String> sorted = versions.stream().map(Object::toString).collect(Collectors.toList());
-        List<String> correct = Arrays.asList("0.4.9", "0.4.10", "0.5.1-rc1", "0.5.1-rc2", "0.5.1-rc2+meta", "1.0.3");
-        Assert.assertTrue("Correct version sorting", sorted.equals(correct));
-    }
+  @Test
+  public void sorting() {
+    List<String> original =
+        Arrays.asList("1.0.3", "0.4.9", "0.4.10", "0.5.1-rc1", "0.5.1-rc2", "0.5.1-rc2+meta");
+    List<Version> versions = original.stream().map(Version::parse).collect(Collectors.toList());
+    Collections.sort(versions);
+    List<String> sorted = versions.stream().map(Object::toString).collect(Collectors.toList());
+    List<String> correct =
+        Arrays.asList("0.4.9", "0.4.10", "0.5.1-rc1", "0.5.1-rc2", "0.5.1-rc2+meta", "1.0.3");
+    Assert.assertTrue("Correct version sorting", sorted.equals(correct));
+  }
 }
